c6037f4426f1bc72cd1e5ef5c177daae7c0db4df
mikhaylovqweasdzxc <mikhaylovqweasdzxc@gmail.com>
2021-10-23 02:54:19 +0300

    [laravel] implement the function to get the users history

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -54,11 +54,39 @@ class AnalyticsController extends Controller
         $analyticsData = Analytics::performQuery(Period::years(1),
             'ga:',
             [
-                'metrics' => 'ga:users,ga:newUsers,ga:sessions,ga:sessionsPerUser,ga:pageviews,ga:pageviewsPerSession,ga:avgSessionDuration,ga:bounceRate',
-                'dimensions' => 'ga:pageTitle',
-                // 'start-date' => '2018-08-01',
-                // 'end-date' => '2018-08-31',
+                'metrics' => 'ga:users',
             ]);
-        return $analyticsData['rows'];
+        $now = Carbon::now();
+        $date = $now->toArray();
+        $monthsData = [];
+        for ($i = 1; $i <= $date['month']; $i++) {
+            $days = cal_days_in_month(CAL_GREGORIAN, $i, $date['year']);
+            $startDate = "{$date['year']}/{$i}/01";
+            $startDate = Carbon::createFromFormat('Y/m/d', $startDate);
+            $endDate = "{$date['year']}/{$i}/{$days}";
+            $endDate = Carbon::createFromFormat('Y/m/d', $endDate);
+            if ($i == $date['month']) {
+                $endDate = $now;
+            }
+
+            $monthUsers = Analytics::performQuery(Period::create($startDate, $endDate), 'ga:', [
+                'metrics' => 'ga:users',
+            ]);
+            if (isset($monthUsers['rows']) && isset($monthUsers['rows'][0])) {
+                array_push($monthsData, $monthUsers['rows'][0][0]);
+            } else {
+                array_push($monthsData, 0);
+            }
+        }
+        $data = [
+            'analyticsData' => [
+                'subscribers' => $analyticsData['rows'][0][0] ? $analyticsData['rows'][0][0] : 0,
+            ],
+            'series' => [
+                'data' => $monthsData,
+                'name' => 'Users',
+            ],
+        ];
+        return response()->json($data);
     }
 }
