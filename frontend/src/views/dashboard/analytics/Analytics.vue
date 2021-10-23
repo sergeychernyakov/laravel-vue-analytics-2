8762ca432be9a64b51e65aeed703010dc7571971
mikhaylovqweasdzxc <mikhaylovqweasdzxc@gmail.com>
2021-10-23 04:41:42 +0300

    [vue] get the visitors and sessions data from the backend

--- frontend/src/views/dashboard/analytics/Analytics.vue
+++ frontend/src/views/dashboard/analytics/Analytics.vue
@@ -15,7 +15,7 @@
           v-if="data.subscribersGained"
           icon="UsersIcon"
           :statistic="kFormatter(data.subscribersGained.analyticsData.subscribers)"
-          statistic-title="Subscribers Gained"
+          statistic-title="Visitors"
           :chart-data="data.subscribersGained.series"
         />
       </b-col>
@@ -64,17 +64,18 @@
 </template>
 
 <script>
-import { BRow, BCol } from 'bootstrap-vue'
+import { BRow, BCol } from "bootstrap-vue";
 
-import StatisticCardWithAreaChart from '@core/components/statistics-cards/StatisticCardWithAreaChart.vue'
-import { kFormatter } from '@core/utils/filter'
-import InvoiceList from '@/views/apps/invoice/invoice-list/InvoiceList.vue'
-import AnalyticsCongratulation from './AnalyticsCongratulation.vue'
-import AnalyticsAvgSessions from './AnalyticsAvgSessions.vue'
-import AnalyticsSupportTracker from './AnalyticsSupportTracker.vue'
-import AnalyticsTimeline from './AnalyticsTimeline.vue'
-import AnalyticsSalesRadarChart from './AnalyticsSalesRadarChart.vue'
-import AnalyticsAppDesign from './AnalyticsAppDesign.vue'
+import StatisticCardWithAreaChart from "@core/components/statistics-cards/StatisticCardWithAreaChart.vue";
+import { kFormatter } from "@core/utils/filter";
+import InvoiceList from "@/views/apps/invoice/invoice-list/InvoiceList.vue";
+import AnalyticsCongratulation from "./AnalyticsCongratulation.vue";
+import AnalyticsAvgSessions from "./AnalyticsAvgSessions.vue";
+import AnalyticsSupportTracker from "./AnalyticsSupportTracker.vue";
+import AnalyticsTimeline from "./AnalyticsTimeline.vue";
+import AnalyticsSalesRadarChart from "./AnalyticsSalesRadarChart.vue";
+import AnalyticsAppDesign from "./AnalyticsAppDesign.vue";
+import axios from "axios";
 
 export default {
   components: {
@@ -92,15 +93,26 @@ export default {
   data() {
     return {
       data: {},
-    }
+    };
   },
-  created() {
+  async created() {
     // data
-    this.$http.get('/analytics/data')
-      .then(response => { this.data = response.data; console.log(response.data) })
+    this.$http.get("/analytics/data").then((response) => {
+      this.data = response.data;
+    });
+    await axios
+      .get("http://localhost:8081/api/analytics/users")
+      .then((response) => {
+        this.data.subscribersGained = response.data;
+      });
+    await axios
+      .get("http://localhost:8081/api/analytics/sessions")
+      .then((response) => {
+        this.data.avgSessions = response.data;
+      });
   },
   methods: {
     kFormatter,
   },
-}
+};
 </script>
